08f42c42eb4692b2c73da4280bdfbfb280b8cbac
Daniel Golitsynski <don.aus@gmail.com>
2023-03-01 15:07:02 +1000

    Simplified template code with accessing elements via dot, rather than a dictionary style

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 menu_items = [
-    {'id': 'id_pizza', 'name': 'name_pizza', 'price' :12.50}, 
-    {'id': 'id_soup', 'name': 'name_soup', 'price': 19.50},
-    {'id': 'id_ravioli', 'name': 'name_ravioli', 'price': 11.11},
+    {'id': 'id_pizza', 'name': 'name_pizza', 'price' :12.50, 'ingredients': 'mozarella, olives, dough'}, 
+    {'id': 'id_soup', 'name': 'name_soup', 'price': 19.50, 'ingredients': 'water, potato'}, 
+    {'id': 'id_ravioli', 'name': 'name_ravioli', 'price': 11.11, 'ingredients': 'ravioli, bread'},
 ]
 
 @app.route('/')
