190488c6c924f030fb6cbdf475e0e47a1cb9a5fc
Daniel Golitsynski <don.aus@gmail.com>
2023-03-07 19:48:54 +1000

    Added sql tables with sqlalchemy

--- app.py
+++ app.py
@@ -1,14 +1,58 @@
+from datetime import datetime
 from flask import Flask, render_template, request
 from flask import flash, redirect, url_for
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
+
 app.config['SECRET_KEY'] = '24ba438773ab9e100d2a985fef34b506'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurant.db'
+
+db = SQLAlchemy(app)
+
+app.app_context().push()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(120), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.id}', '{self.email}', '{self.image_file}', '{self.password}')"
+
+class MenuItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.String(30), nullable=False)
+    price = db.Column(db.Float, nullable=False, default=0.0)
+    description = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"MenuItem('{self.id}', '{self.name}', '{self.price}', '{self.description}')"
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Order('{self.id}', '{self.user_id}', '{self.date_time}')"
+
+class OrderWithMenuItems(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    menu_item_id = db.Column(db.Integer, db.ForeignKey('menu_item.id'), nullable=False)
+
+    def __repr__(self):
+        return f"OrderWithMenuItem('{self.id}', '{self.order_id}', '{self.menu_item_id}')"
+    
 
 menu_items = [
-    {'id': 'id_pizza', 'name': 'name_pizza', 'price' :12.50, 'ingredients': 'mozarella, olives, dough'}, 
-    {'id': 'id_soup', 'name': 'name_soup', 'price': 19.50, 'ingredients': 'water, potato'}, 
+   
+   
     {'id': 'id_ravioli', 'name': 'name_ravioli', 'price': 11.11, 'ingredients': 'ravioli, bread'},
 ]
 
